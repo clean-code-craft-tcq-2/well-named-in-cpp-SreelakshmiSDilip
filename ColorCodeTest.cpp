0ae3aaea671b3889165d5e2f07500318d97f8a80
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-07 18:07:41 +0530

    Update ColorCodeTest.cpp

--- ColorCodeTest.cpp
+++ ColorCodeTest.cpp
@@ -30,7 +30,7 @@ void testPairToNumber(
 
  void PrintColorCodingReferenceManual()
      {
-	     std::cout <<"std::endl"<<*****Color Coding Reference Manual*****"<<std::endl<< "PairNumber " << "Major"<<"  " <<"Minor"<< std::endl;
+	     std::cout <<"std::endl"<<"*****Color Coding Reference Manual*****"<<std::endl<< "PairNumber " << "Major"<<"  " <<"Minor"<< std::endl;
 	     for(uint pairNum = 1; pairNum <= c_MAXPAIRNUM; pairNum++)
 	     {
 		     TelCoColorCoder::ColorPair currentColorPair = TelCoColorCoder::GetColorFromPairNumber(pairNum);

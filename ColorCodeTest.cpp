e14afef1abb76faf3d51268a0a9de08ed393c82b
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-07 18:03:46 +0530

    Update ColorCodeTest.cpp

--- ColorCodeTest.cpp
+++ ColorCodeTest.cpp
@@ -52,7 +52,7 @@ int main() {
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-    TelCoColorCoder::PrintColorCodingReferenceManual();
+    PrintColorCodingReferenceManual();
 
     return 0;
 }

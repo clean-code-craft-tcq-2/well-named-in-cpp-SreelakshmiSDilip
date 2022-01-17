54cd72cbdf502dccc3496cb6cd034a2eed7b1ae9
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-17 11:36:08 +0530

    Update and rename PrintColorCodingReferenceManual.cpp to ColorCodingReferenceManual.cpp

---
+++ ColorCodingReferenceManual.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "ColorCode.h"
+
+void PrintColorCodingReferenceManual()
+{
+
+    std::cout <<std::endl<<"*****Color Coding Reference Manual*****"<<std::endl<< "  PairNumber Major  Minor"<< std::endl;
+    for(uint pairNum = 1; pairNum <= c_MAXPAIRNUM; pairNum++)
+    {
+        TelCoColorCoder::ColorPair currentColorPair = TelCoColorCoder::GetColorFromPairNumber(pairNum);
+        if(pairNum < c_TWODIGITSTARTINGINDEX)
+        {
+            std::cout <<sixSpaces<<pairNum<<sixSpaces<<currentColorPair.ToString()<< std::endl;
+        }
+        else
+        {
+            std::cout <<fiveSpaces<<pairNum<<sixSpaces<< currentColorPair.ToString()<< std::endl;
+        }     
+    }
+}

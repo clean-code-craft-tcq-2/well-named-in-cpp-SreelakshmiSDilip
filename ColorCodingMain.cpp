e79f0663e1b4ce52cfa4f3a35ac414ce1fd9d523
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-17 20:07:13 +0530

    Update ColorCodingMain.cpp

--- ColorCodingMain.cpp
+++ ColorCodingMain.cpp
@@ -33,6 +33,6 @@ int main() {
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-    testColorCodingReferenceManual(6,"6      Red    Blue" );
+    testColorCodingReferenceManual(6,"      6      Red    Blue" );
     return 0;
 }

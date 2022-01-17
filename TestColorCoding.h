dcda662b2be80d56122d94e305b62e89b4a048a0
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-17 19:05:05 +0530

    Update TestColorCoding.h

--- TestColorCoding.h
+++ TestColorCoding.h
@@ -3,5 +3,7 @@
 #include "ColorCode.h"	
 
 void testNumberToPair(int pairNumber,TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
+
 void testPairToNumber(TelCoColorCoder::MajorColor major,TelCoColorCoder::MinorColor minor,int expectedPairNumber);
+
 void testColorCodingReferenceManual();

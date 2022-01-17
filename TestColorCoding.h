ff26cdadddfc6374db36c7ec6d6b31e464285743
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-17 18:53:21 +0530

    Update TestColorCoding.h

--- TestColorCoding.h
+++ TestColorCoding.h
@@ -1,5 +1,7 @@
-	
+#include <iostream>
+#include <assert.h>
+#include"ColorCode.h"	
 
-  void testNumberToPair(int pairNumber,TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
-	void testPairToNumber(TelCoColorCoder::MajorColor major,TelCoColorCoder::MinorColor minor,int expectedPairNumber);
-	void testColorCodingReferenceManual();
+void testNumberToPair(int pairNumber,TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
+void testPairToNumber(TelCoColorCoder::MajorColor major,TelCoColorCoder::MinorColor minor,int expectedPairNumber);
+void testColorCodingReferenceManual();

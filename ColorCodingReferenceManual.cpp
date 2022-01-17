6760139f0c65e21fd17e7ce32e9e81261e473271
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-17 20:12:13 +0530

    Update ColorCodingReferenceManual.cpp

--- ColorCodingReferenceManual.cpp
+++ ColorCodingReferenceManual.cpp
@@ -2,14 +2,16 @@
 #include "ColorCode.h"
 
 
-void PrintColorCodingReferenceManual()
+namespace TelCoColorCoder
 {
-    std::cout <<std::endl<<"*****Color Coding Reference Manual*****"<<std::endl<< "  PairNumber Major  Minor"<< std::endl;
-    for(uint pairNum = 1; pairNum <= c_MAXPAIRNUM; pairNum++)
-    {
-        std::string strCurrentRowEntry = GetCombinedPairNumandColorString(pairNum);
-        std::cout <<strCurrentRowEntry<< std::endl;
+	void PrintColorCodingReferenceManual()
+	{
+		std::cout <<std::endl<<"*****Color Coding Reference Manual*****"<<std::endl<< "  PairNumber Major  Minor"<< std::endl;
+		for(uint pairNum = 1; pairNum <= c_MAXPAIRNUM; pairNum++)
+		{
+			std::string strCurrentRowEntry = GetCombinedPairNumandColorString(pairNum);
+			std::cout <<strCurrentRowEntry<< std::endl;
 
-    }
+		}
+	}
 }
-

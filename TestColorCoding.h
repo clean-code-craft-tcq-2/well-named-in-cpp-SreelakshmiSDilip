084447cb5aa4e3bcb7397402d7ad321a80284080
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-17 18:52:05 +0530

    Create TestColorCoding.h

---
+++ TestColorCoding.h
@@ -0,0 +1,5 @@
+	
+
+  void testNumberToPair(int pairNumber,TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
+	void testPairToNumber(TelCoColorCoder::MajorColor major,TelCoColorCoder::MinorColor minor,int expectedPairNumber);
+	void testColorCodingReferenceManual();

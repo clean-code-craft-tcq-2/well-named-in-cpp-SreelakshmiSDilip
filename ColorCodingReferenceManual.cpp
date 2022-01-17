fa23e075dc7a14e9731045f238e6a106192ee53f
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-01-17 11:46:46 +0530

    Update ColorCodingReferenceManual.cpp

--- ColorCodingReferenceManual.cpp
+++ ColorCodingReferenceManual.cpp
@@ -9,14 +9,15 @@ namespace TelCoColorCoder
         std::cout <<std::endl<<"*****Color Coding Reference Manual*****"<<std::endl<< "  PairNumber Major  Minor"<< std::endl;
         for(uint pairNum = 1; pairNum <= c_MAXPAIRNUM; pairNum++)
         {
-            TelCoColorCoder::ColorPair currentColorPair = TelCoColorCoder::GetColorFromPairNumber(pairNum);
+            ColorPair currentColorPair = TelCoColorCoder::GetColorFromPairNumber(pairNum);
+            std::string currentColorPairString = currentColorPair.ToString();
             if(pairNum < c_TWODIGITSTARTINGINDEX)
             {
-                std::cout <<sixSpaces<<pairNum<<sixSpaces<<currentColorPair.ToString()<< std::endl;
+                std::cout <<sixSpaces<<pairNum<<sixSpaces<<currentColorPairString<< std::endl;
             }
             else
             {
-                std::cout <<fiveSpaces<<pairNum<<sixSpaces<< currentColorPair.ToString()<< std::endl;
+                std::cout <<fiveSpaces<<pairNum<<sixSpaces<<currentColorPairString<< std::endl;
             }     
         }
     }
